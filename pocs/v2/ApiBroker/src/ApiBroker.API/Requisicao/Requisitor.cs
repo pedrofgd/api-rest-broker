69bfba5b6af99cffd037473d86ca91121c882bd8
An-225 <66622436+An-225@users.noreply.github.com>
2023-03-29 21:33:34 -0300

    Fix do fix merge :/

--- pocs/v2/ApiBroker/src/ApiBroker.API/Requisicao/Requisitor.cs
+++ pocs/v2/ApiBroker/src/ApiBroker.API/Requisicao/Requisitor.cs
@@ -2,17 +2,11 @@ namespace ApiBroker.API.Requisicao;
 
 public class Requisitor
 {
-    private void LogResultado(SolicitacaoDto solicitacao, ProvedorSettings provedorAlvo,
-        HttpResponseMessage respostaProvedor, long tempoRespostaMs)
+    private async Task<Tuple<HttpResponseMessage, long>> EnviarRequisicaoProvedor(HttpRequestMessage requisicao)
     {
-        var monitorador = new Monitorador();
-        var logDto = new LogDto
-        {
-            NomeRecurso = solicitacao.Nome, 
-            NomeProvedor = provedorAlvo.Nome,
-            TempoRespostaMs = tempoRespostaMs,
-            Sucesso = respostaProvedor.IsSuccessStatusCode
-        };
-        monitorador.Log(logDto);
+        var watch = Stopwatch.StartNew();
+        var httpClient = new HttpClient();
+        var resultado = await httpClient.SendAsync(requisicao); 
+        return new Tuple<HttpResponseMessage, long>(resultado, watch.ElapsedMilliseconds);
     }
 }
