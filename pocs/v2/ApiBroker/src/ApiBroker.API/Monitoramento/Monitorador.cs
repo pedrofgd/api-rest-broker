7b53f288cf0ad3e293aecfbde6cab6175c35f89d
An-225 <66622436+An-225@users.noreply.github.com>
2023-04-16 21:47:01 -0300

    Logs assíncronos

--- pocs/v2/ApiBroker/src/ApiBroker.API/Monitoramento/Monitorador.cs
+++ pocs/v2/ApiBroker/src/ApiBroker.API/Monitoramento/Monitorador.cs
@@ -6,19 +6,11 @@ namespace ApiBroker.API.Monitoramento;
 
 public class Monitorador
 {
-    /*
-     * todo: substituir pelo Token gerado ao rodar o Influx localmente
-     *  Depois colocar como variável ambiente..
-     */
-
     public void Log(LogDto logDto)
     {
-        // todo: ajustar para utilizar o InfluxDbClientFactory
-        
-        // todo: url do Influx como variável ambiente
         using var influx = InfluxDbClientFactory.OpenConnection();
-        using var writeApi = influx.GetWriteApi();
-        
+        var writeApi = influx.GetWriteApiAsync();
+
         var point = PointData.Measurement("metricas_recursos")
             .Tag("nome_recurso", logDto.NomeRecurso)
             .Tag("nome_provedor", logDto.NomeProvedor)
@@ -27,12 +19,8 @@ public class Monitorador
             .Field("sucesso", logDto.Sucesso ? 1 : 0)
             .Timestamp(DateTime.UtcNow, WritePrecision.Ms);
         
-        /*
-         * todo: timeout aqui está passando batido...
-         *  Quando o servidor do InfluxDB não está rodando, a aplicação tenta
-         *  por alguns segundos e depois segue a execução, mesmo que não tenha gravado.
-         *  Ver como lidar com erros e timeout
-         */
-        writeApi.WritePoint(point, "logs", "broker");
+#pragma warning disable CS4014
+        writeApi.WritePointAsync(point, "logs", "broker");
+#pragma warning disable CS4014
     }
 }
